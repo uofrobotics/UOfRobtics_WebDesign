47eac720079914cf40d1587de54c1a0f4431292a
COde-Tigg3r <justusdab@gmail.com>
2024-10-02 16:29:05 -0600

    yoo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from '../Code/components/Navbar';
 import './Code_css/App.css';
-import Home from './components/pages/Home';
+import Home from '../Code/components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import About from './components/pages/About'; // this is the about page 
-import Events from './components/pages/Events';// this is the event page
-import Projects from './components/pages/Projects'; // this is the projects page
+import About from '../Code/components/pages/About'; // this is the about page 
+import Events from '../Code/components/pages/Events';// this is the event page
+import Projects from '../Code/components/pages/Projects'; // this is the projects page
 
 
 function App() {
