3c83719e56525e8daf11b88e5971424bbf653ebe
Ben <benshupe@sasktel.net>
2024-10-07 23:57:28 -0600

    change social links to logos

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import facebook_logo from '../images/facebook_logo.png'
+import instagram_logo from '../images/instagram_logo.png'
+import twitter_logo from '../images/twitter_logo.png'
 import './Components_css/Footer.css';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
@@ -7,8 +10,10 @@ function Footer() {
   return(
 <footer className="footer-container">
       <p>Contact us at: support@uofrobotics.com</p>
-      <div className="social-media-links">
-        <a href="#">Facebook</a> | <a href="#">Instagram</a> | <a href="#">Twitter</a>
+      <div className="social-icons">
+        <a class="social-icon-link" href="#"><img class="social-logo"  src={facebook_logo} /></a>
+        <a class="social-icon-link" href="#"><img class="social-logo"  src={instagram_logo} /></a>
+        <a class="social-icon-link" href="#"><img class="social-logo"  src={twitter_logo} /></a>
       </div>
     </footer>
   ); 
