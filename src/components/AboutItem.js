dba258bec838f3c455624c52c11c2790a1ea78b7
COde-Tigg3r <justusdab@gmail.com>
2025-02-22 15:51:46 -0600

    done

--- src/components/AboutItem.js
+++ src/components/AboutItem.js
@@ -4,15 +4,13 @@ import './Components_css/About_Cards.css';
 
 function AboutItem(props) {
   return (
-    <>
-      <li className="about__item">
-        <Link className="about__item__link" to={props.path}>
-          <figure className="about__item__pic__wrap">
-            <img src={props.src} alt={props.alt} className="about__item__img" />
-          </figure>
-        </Link>
-      </li>
-    </>
+    <li className="about__item">
+      <Link className="about__item__link" to={props.path}>
+        <figure className="about__item__pic__wrap">
+          <img src={props.src} alt={props.alt} className="about__item__img" />
+        </figure>
+      </Link>
+    </li>
   );
 }
 
