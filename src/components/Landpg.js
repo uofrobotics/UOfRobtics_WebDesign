eebb31ea89361b0a0c847d4d4011d301e16e0da7
Vydeh <142721431+Vydeh07@users.noreply.github.com>
2025-09-17 23:35:12 +0530

    fix: update Join button to redirect to Discord (#26)

--- src/components/Landpg.js
+++ src/components/Landpg.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../components/Components_css/App.css';
-import { Button } from './Button';
-import RegistrationModal from './RegistrationModal';
+// import { Button } from './Button';
+// import RegistrationModal from './RegistrationModal';
 import './Components_css/Land_pg.css';
 import newLogo from '../svg/Selection.svg';
 
 function Landpg() {
-  const [showModal, setShowModal] = useState(false);
-
-  const openModalHandler = () => setShowModal(true);
-  const closeModalHandler = () => setShowModal(false);
+ 
+  // const [showModal, setShowModal] = useState(false);
+  // const openModalHandler = () => setShowModal(true);
+  // const closeModalHandler = () => setShowModal(false);
 
   return (
     <>
@@ -23,17 +23,18 @@ function Landpg() {
         <p>What are you waiting for?</p>
 
         <div className='hero-btns'>
-          <Button
-            className='btns'
-            buttonStyle='btn--outline'
-            buttonSize='btn--large'
-            onClick={openModalHandler}
+          
+          <a
+            href="https://discord.gg/wtTFD3qRTM"
+            className='btns btn--outline btn--large'
+            target="_blank" 
+            rel="noopener noreferrer"
           >
             JOIN
-          </Button>
+          </a>
         </div>
       </div>
-      <RegistrationModal show={showModal} onClose={closeModalHandler} />
+      
     </>
   );
 }
